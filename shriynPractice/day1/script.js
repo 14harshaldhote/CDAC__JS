7c39aa4fc567257c0ef57f4782801befd5b1d2b1
Harshal Dhote <dhoteh020@gmail.com>
2023-12-01 08:24:51 +0530

    com

---
+++ shriynPractice/day1/script.js
@@ -0,0 +1,30 @@
+
+    // function ab(){
+    //     var x=12;
+    //     return function(){
+    //         console.log(x);
+    //     }
+    // }
+
+    // var ans=ab();
+    // ans();
+
+
+
+    //day2
+    var a;
+    var a1=12;
+
+    console.log();
+
+
+// Original code snippet
+// var a;
+// var a1 = 12;
+// console.log();
+
+// Modified code to make one variable or expression log null
+var a = null;   // Assigning null to the variable 'a'
+var a1 = 12;
+
+console.log(a);  // This will log null to the console
